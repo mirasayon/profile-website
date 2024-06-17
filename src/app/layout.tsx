db1fefab6fbf75e6eeeef77254c4a235a5863393
mirasayon <persocountone@gmail.com>
2024-06-17 16:33:38 +0500

    initial commit, deploying to v*.

---
+++ src/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import "../styles/globals.css";
+import localFont from "next/font/local";
+import { GoogleAnalytics } from "@next/third-parties/google";
+
+const lFont = localFont({
+  src: "./fonts/0xProtoNerdFontMono-Regular.woff2",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "MiraSayon",
+  description: "Welcome to my profile!",
+};
+const gaid = process.env.GAID;
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={lFont.className}>
+        <a href="https://nodejs.org/en" target="_blank">
+          <img
+            src="/nodejstutleart.png"
+            className="turtle"
+            width={100}
+            height={100}
+            alt=""
+          />
+        </a>
+        <a href="https://deno.com" target="_blank">
+          <img
+            src="/denoland.png"
+            className="deno"
+            width={100}
+            height={100}
+            alt=""
+          />
+        </a>
+
+        {children}
+        {gaid ? "gae" : "gane"}
+      </body>
+      {gaid && <GoogleAnalytics gaId={gaid} />}
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,61 @@
+import styles from "../styles/page.module.css";
+
+export default function RootPage() {
+  const logoSizes = 50;
+  return (
+    <main className={styles.root}>
+      <h1>Hiii, I'm Mirasayon</h1>
+      <p>
+        Developer and shinobi. Node.js/Deno Fullstack developer from
+        Konohagakure. Developer of <a href="https://reanime.art">reanime.art</a>
+      </p>
+      <span>
+        JavaScript, oh yeah, your skin and bones <br />
+        Turn into something beautiful <br />
+        And you know, you know I love you so
+        <br /> You know I love you so
+      </span>
+      <p>Contacts</p>
+      <div className={styles.links}>
+        <a href="https://reanime.art">
+          <img
+            className={styles.rlogo}
+            src="/icon.png"
+            alt=""
+            width={logoSizes}
+            height={logoSizes}
+          />
+        </a>
+        <a href="https://t.me/miragesayon">
+          <img
+            className={styles.logo}
+            src="/tglogo.png"
+            alt=""
+            width={logoSizes}
+            height={logoSizes}
+          />
+        </a>
+
+        <a href="https://x.com/mirasayon">
+          <img
+            className={styles.logo}
+            src="/xlogo.png"
+            alt=""
+            width={logoSizes}
+            height={logoSizes}
+          />
+        </a>
+        <a href="https://github.com/mirasayon">
+          <img
+            className={styles.logo}
+            src="/ghlogo.png"
+            alt=""
+            width={logoSizes}
+            height={logoSizes}
+          />
+        </a>
+      </div>
+      <img src="/bggifdef.gif" width={1280 / 2} height={720 / 2} />
+    </main>
+  );
+}
